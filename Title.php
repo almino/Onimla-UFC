32a6ab1928d3796adf6470372b26dd2c9bb858e3
Almino Melo <almino.melo@gmail.com>
2016-06-06 16:37:27 -0300

    Altera o separador

--- Title.php
+++ Title.php
@@ -17,26 +17,28 @@ class Title extends \Onimla\HTML\Element {
 
     public function __toString() {
         $pre = NULL;
-        
+
         if (strlen(self::$controller) > 0 AND strlen(self::$view) < 1) {
             $pre = self::$controller;
         } elseif (strlen(self::$view) > 0 AND strlen(self::$controller) > 0) {
             $pre = self::$view . ', ' . self::$controller;
         }
-        
+
         if (strlen($pre) > 0) {
             $this->appendText($pre);
             $this->append(' ', self::$separator, ' ');
         }
-        
+
         if (strlen(self::$short) > 0) {
-            $this->appendText(self::$short, ' | ');
+            $this->appendText(self::$short);
+            $this->append(' ', self::$separator, ' ');
         } elseif (strlen(self::$long) > 0) {
-            $this->appendText(self::$long, ' | ');
+            $this->appendText(self::$long);
+            $this->append(' ', self::$separator, ' ');
         }
-        
+
         $this->appendText(self::$ending);
-        
+
         return parent::__toString();
     }
 
